879cbd406851cea05a430e67ad0e2b0c6f358f33
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-04-22 15:27:57 -0700

    added increment of 15 and prevents from booking before present time

--- src/components/RealTimeInterface/TimePicker.vue
+++ src/components/RealTimeInterface/TimePicker.vue
@@ -1,32 +1,41 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import Datepicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
 
 const date = ref(null);
 
+// Set the current date and time
+const today = new Date();
+
 const bookDate = () => {
     if (date.value) {
-        console.log(`Booking for: ${date.value.toLocaleString()}`);
+        console.log(`Booking for: ${date.value}`);
     }
 };
+
+const formattedDate = computed(() => {
+    if (date.value) {
+        return `${date.value}`;
+    }
+    return '';
+});
 </script>
 
 <template>
     <p class="date-text">Select a date and time:</p>
     <div class="datepicker">
-      <Datepicker v-model="date" />
+      <Datepicker v-model="date" :min-date="today" minutes-increment="15"/>
     </div>
-    <p v-if="date" class="selected-date">Selected Date: {{ date.toLocaleString() }}</p>
+    <p v-if="date" class="selected-date">Selected for {{ formattedDate }}</p>
     <v-btn variant="tonal" color="indigo" v-if="date" @click="bookDate">Book</v-btn>
 </template>
 
 <style scoped>
 .date-text, .selected-date {
     font-size: 1em;
-    margin-bottom: 1em;
     text-align: left;
-    margin-left: 7em;
+    margin: 1em 0 1em 7em;
 }
 .datepicker {
     display: flex;
