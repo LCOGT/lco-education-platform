e3356c79fe0e49a5b63cb7b2d4bad3de441a46cc
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-04-22 14:37:50 -0700

    imports vuetify

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 import router from './router/index'
 import App from './App.vue'
-
-
+import vuetify from './plugins/vuetify'
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedState)
 
+
 createApp(App)
   .use(pinia)
   .use(router)
+  .use(vuetify)
   .mount('#app')
