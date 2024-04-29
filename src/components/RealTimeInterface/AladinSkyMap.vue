b34ce0bddead184a75da990d58251de9c132cb33
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-04-29 12:04:47 -0700

    added logic to parse ra and dec into coordinates to move Aladin to user's entry

--- src/components/RealTimeInterface/AladinSkyMap.vue
+++ src/components/RealTimeInterface/AladinSkyMap.vue
@@ -3,6 +3,7 @@ import { onMounted, ref } from 'vue'
 import { initializeAladin } from '../../utils/aladinUtility.js'
 
 const aladinContainer = ref(null)
+let aladinInstance = null
 
 onMounted(() => {
   initializeAladin(aladinContainer.value, {
@@ -16,10 +17,49 @@ onMounted(() => {
   })
     .then(aladin => {
       console.log('Aladin initialized:', aladin)
+      aladinInstance = aladin
     })
     .catch(error => {
       console.error('Failed to initialize Aladin:', error)
     })
+})
+
+function parseRa (raString) {
+  const coordinates = raString.toString().match(/(\d+.*)h (\d+.*)m (\d+.*)s/)
+  if (!coordinates) return null
+
+  const hours = parseInt(coordinates[1], 10)
+  const minutes = parseInt(coordinates[2], 10)
+  const seconds = parseInt(coordinates[3], 10)
+
+  return 15 * (hours + minutes / 60 + seconds / 3600)
+}
+
+function parseDec (decString) {
+  const coordinates = decString.toString().match(/([+-]?\d+.*)°\s(\d+.*)['′]\s(\d+.*)[″"].*/)
+  if (!coordinates) return null
+
+  const degrees = parseInt(coordinates[1], 10)
+  const arcminutes = parseInt(coordinates[2], 10)
+  const arcseconds = parseInt(coordinates[3], 10)
+
+  return degrees + (degrees < 0 ? -1 : 1) * (arcminutes / 60 + arcseconds / 3600)
+}
+
+function goToRaDec (ra, dec) {
+  const raNumber = parseRa(ra)
+  const decNumber = parseDec(dec)
+  console.log('RA:', raNumber, 'DEC:', decNumber)
+
+  if (aladinInstance && raNumber && decNumber) {
+    aladinInstance.gotoRaDec(raNumber, decNumber)
+  } else {
+    console.error('Invalid RA/DEC values or Aladin instance not initialized')
+  }
+}
+
+defineExpose({
+  goToRaDec
 })
 </script>
 
