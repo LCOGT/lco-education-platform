283dff052014bc965b8ec770e1f8d2f24e2dba92
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-04-30 13:06:27 -0700

    updated aladin from v2 to v3

--- src/utils/aladinUtility.js
+++ src/utils/aladinUtility.js
@@ -3,34 +3,41 @@ function loadScript (url, callback) {
     const script = document.createElement('script')
     script.src = url
     script.onload = () => resolve(callback ? callback() : undefined)
-    script.onerror = reject
+    script.onerror = () => reject(new Error(`Failed to load script at ${url}`))
     document.head.appendChild(script)
   })
 }
 
 export function loadAladinScript () {
   return new Promise((resolve, reject) => {
-    if (typeof jQuery === 'undefined') {
-      loadScript('https://code.jquery.com/jquery-3.6.0.min.js', () => {
-        loadAladin().then(resolve).catch(reject)
-      }).catch(reject)
-    } else {
-      loadAladin().then(resolve).catch(reject)
-    }
-  })
-}
-
-function loadAladin () {
-  return loadScript('https://aladin.u-strasbg.fr/AladinLite/api/v2/latest/aladin.min.js', () => {
-    if (typeof A !== 'undefined' && A.aladin) {
-      return A
-    } else {
-      throw new Error("Aladin script loaded but 'A' or 'A.aladin' is undefined.")
-    }
+    loadScript('https://code.jquery.com/jquery-1.12.1.min.js')
+      .then(() => {
+        return loadScript('https://aladin.u-strasbg.fr/AladinLite/api/v3/latest/aladin.js')
+      })
+      .then(() => {
+        if (typeof A !== 'undefined' && A.aladin && A.init) {
+          A.init.then(() => {
+            resolve(A)
+          }).catch((initError) => {
+            throw new Error(`Error during Aladin initialization: ${initError}`)
+          })
+        } else {
+          throw new Error("Aladin script loaded but 'A' or 'A.aladin' or 'A.init' is undefined.")
+        }
+      })
+      .catch(error => {
+        console.error('Error loading scripts: ', error)
+        reject(error)
+      })
   })
 }
 
 export async function initializeAladin (el, options) {
-  const A = await loadAladinScript()
-  return new A.aladin(el, options)
+  try {
+    const A = await loadAladinScript()
+    const aladinInstance = new A.aladin(el, options)
+    return aladinInstance
+  } catch (error) {
+    console.error('Failed to initialize Aladin:', error)
+  }
 }
