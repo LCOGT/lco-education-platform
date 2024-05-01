62204147b8af99c28a928e58e91bf553e0cca890
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-05-01 13:53:26 -0700

    imported material design icons

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 import router from './router/index'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import 'material-icons/iconfont/material-icons.css'
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedState)
