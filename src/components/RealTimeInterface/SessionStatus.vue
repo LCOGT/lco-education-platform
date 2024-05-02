4f16b9e45d8687e8359c0d35e4f97c376cc159dd
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-05-02 16:08:24 -0700

    successfully unnested components

--- src/components/RealTimeInterface/SessionStatus.vue
+++ src/components/RealTimeInterface/SessionStatus.vue
@@ -1,11 +1,13 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, defineEmits } from 'vue'
 import SessionPending from './SessionPending.vue'
 import SessionStarted from './SessionStarted.vue'
 
+const emits = defineEmits(['update-session-status'])
 const sessionStatus = ref('pending')
 
 const updateSessionStatus = (newStatus) => {
+  emits('update-session-status', newStatus)
   sessionStatus.value = newStatus
 }
 </script>
--- src/components/Views/RealTimeInterfaceView.vue
+++ src/components/Views/RealTimeInterfaceView.vue
@@ -1,10 +1,21 @@
 <script setup>
+import { ref } from 'vue'
 import TimePicker from '../RealTimeInterface/TimePicker.vue'
+import SessionPending from '../RealTimeInterface/SessionPending.vue'
+import SessionStarted from '../RealTimeInterface/SessionStarted.vue'
+
+const currentView = ref('scheduling')
+
+const handleViewChange = (view) => {
+  currentView.value = view
+}
 
 </script>
 
 <template>
     <div>
-        <TimePicker />
+        <TimePicker v-if="currentView === 'scheduling'" @changeView="handleViewChange" />
+        <SessionPending v-else-if="currentView === 'sessionpending'" @changeView="handleViewChange" />
+        <SessionStarted v-else-if="currentView === 'sessionstarted'" @changeView="handleViewChange" />
     </div>
 </template>
