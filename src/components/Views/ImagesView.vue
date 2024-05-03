b04a6c490dc8f0ae8a1f5e75c8fbd2c784fa5727
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-05-03 11:08:26 -0700

    styled imagesview

--- src/components/Views/ImagesView.vue
+++ src/components/Views/ImagesView.vue
@@ -1,11 +1,35 @@
 <script setup>
 import MyGallery from '../Images/MyGallery.vue'
+
 </script>
 
 <template>
-  <div>
-    <h2>Photon Ranch Gallery</h2>
-    <v-btn color="indigo">Image Archive</v-btn>
-    <MyGallery />
+  <div class="gallery-container">
+    <div class="gallery-header">
+        <h2>Photon Ranch Gallery</h2>
+        <v-btn color="indigo"><a href="https://archive.photonranch.org/" target="_blank" class="ptr-archive">Image Archive</a></v-btn>
+    </div>
+    <div class="gallery-content">
+      <MyGallery />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.gallery-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.gallery-header {
+  margin-bottom: 1em;
+}
+.ptr-archive {
+  text-decoration: none;
+  color:white;
+}
+.gallery-content {
+  display: flex;
+  width: 50%;
+}
+</style>
