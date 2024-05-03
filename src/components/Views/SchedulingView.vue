6295c2827522daf60a70cceb21a7730241e9c578
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-05-03 14:40:03 -0700

    simple scheduling view that renders two differnt views based on selection

---
+++ src/components/Views/SchedulingView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+const level = ref('')
+
+</script>
+
+<template>
+    <div v-if="!level" class="level-buttons-wrapper">
+        <p>What level are you?</p>
+        <v-btn @click="level = 'beginner'" color="indigo" class="level-btns">Beginner</v-btn>
+        <v-btn @click="level = 'expert'" color="indigo" class="level-btns">Expert</v-btn>
+    </div>
+    <div v-else-if="level === 'beginner'">
+        <p>Beginner level</p>
+    </div>
+    <div v-else-if="level === 'expert'">
+        <p>Expert Mode</p>
+    </div>
+</template>
+
+<style scoped>
+.level-buttons-wrapper {
+    margin: 1em;
+    gap: 1em;
+}
+.level-btns {
+    margin: 1em;
+}
+</style>
