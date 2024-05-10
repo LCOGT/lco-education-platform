dae5f46dcfbde3f4c5d7b4275471c35d5423cce1
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-05-10 15:14:11 -0700

    Created telescope selection component

---
+++ src/components/Scheduling/TelescopeSelection.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+const telescope = ref('')
+
+const saveTelescope = (selectedTelescope) => {
+  telescope.value = selectedTelescope
+}
+</script>
+
+<template>
+    <div>
+      <label for="telescope">telescope</label>
+      <select v-model="telescope" class="scheduling-inputs">
+        <option disabled value="">Select telescope</option>
+        <option value="Mountain Ranch">Mountain Ranch</option>
+        <option value="Luther College">Luther College</option>
+        <option value="Apache Ridge">Apache Ridge</option>
+        <option value="Eltham College">Eltham College</option>
+      </select>
+      <v-btn color="indigo" :disabled="!telescope" @click="saveTelescope" >Save</v-btn>
+    </div>
+  </template>
