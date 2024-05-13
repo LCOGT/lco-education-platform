e7f8285a38f8a0f9505a26d83cedf4aee2023800
Edward Gomez <edward@gomez.me.uk>
2024-05-13 16:32:45 +0100

    Fixing image sizes

--- src/components/Images/MyGallery.vue
+++ src/components/Images/MyGallery.vue
@@ -15,9 +15,9 @@ const observations = ref([
 <template>
     <div class="container" v-for="obs in observations" :key="obs.id">
         <h3>{{ obs.date }}</h3>
-        <div class="grid">
-        <div class="cell"  v-for="n in obs.num" :key="n">
-            <figure class="image">
+        <div class="columns is-multiline">
+            <div class="column is-one-quarter-desktop is-half-tablet" v-for="n in obs.num" :key="n">
+            <figure class="image is-square">
                 <img :src="thumbnail" class="thumbnail" />
             </figure>
         </div>
