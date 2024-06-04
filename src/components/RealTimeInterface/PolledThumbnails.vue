2b17b8489a2512e92856ab18953d8d7cf8fd1ff4
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-06-03 17:20:10 -0700

    got rid of console logs

--- src/components/RealTimeInterface/PolledThumbnails.vue
+++ src/components/RealTimeInterface/PolledThumbnails.vue
@@ -8,13 +8,10 @@ let pollingInterval = null
 const thumbnailsApiUrl = 'http://archive-api-dev.lco.gtn/thumbnails/?frame_basename=&proposal_id=&observation_id=617904267&request_id=&size=large'
 
 function getThumbnails () {
-  console.log('here')
   fetch(thumbnailsApiUrl)
     .then(response => response.json())
     .then(data => {
-      console.log('this is data', data)
       thumbnails.value = data.results.map(result => result.url)
-      console.log('this is thumbnails', thumbnails.value)
     })
     .catch(error => {
       console.error('Error:', error)
