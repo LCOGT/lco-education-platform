a3d4bd4771a55ed44b537c0d2232d2ccd7b4d7da
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-06-19 16:39:58 -0700

    added api call to block rti but time is hardcoded as are other keys

--- src/components/RealTimeInterface/TimePicker.vue
+++ src/components/RealTimeInterface/TimePicker.vue
@@ -2,16 +2,21 @@
 import { ref, computed, watch, defineEmits } from 'vue'
 import { useRouter } from 'vue-router'
 import { useSessionsStore } from '../../stores/sessions'
+import { useUserDataStore } from '../../stores/userData'
+import { fetchApiCall } from '../../utils/api'
 import LeafletMap from './GlobeMap/LeafletMap.vue'
 
 const router = useRouter()
 const sessionsStore = useSessionsStore()
+const userDataStore = useUserDataStore()
+
+const apitoken = userDataStore.authToken
 
 const date = ref(null)
 const time = ref(null)
 const emits = defineEmits(['changeView'])
 
-const formattedDate = computed(() => {
+const toIsoDate = computed(() => {
   if (date.value) {
     const options = { year: 'numeric', month: 'long', day: 'numeric' }
     return date.value.toLocaleDateString('en-US', options)
@@ -31,6 +36,26 @@ const resetSession = () => {
   sessionsStore.currentSessionId = null
 }
 
+const blockRti = async () => {
+  // const selectedDate = new Date(date.value).toISOString().split('T')[0]
+  // const startDate = `${selectedDate}T${time.value}:00Z`
+  // const endDate = `${selectedDate}T${time.value}:50Z`
+  const requestBody = {
+    proposal: 'LCOSchedulerTest',
+    name: 'Test Real Time',
+    site: 'tst',
+    enclosure: 'doma',
+    telescope: '1m0a',
+    start: '2024-06-29T00:00:00Z',
+    end: '2024-06-29T00:30:00Z'
+  }
+  await fetchApiCall({ url: 'http://observation-portal-dev.lco.gtn/api/realtime/', method: 'POST', body: requestBody, successCallback: () => router.push('/dashboard'), failCallback: handleError })
+}
+
+const handleError = (error) => {
+  console.error('API call failed with error:', error)
+}
+
 const bookDate = () => {
   const selectedSite = sessionsStore.selectedSite
   if (date.value && time.value && selectedSite) {
@@ -45,7 +70,7 @@ const bookDate = () => {
       type: 'realtime'
     }
     sessionsStore.addSession(newSession)
-    router.push('/dashboard')
+    blockRti()
     // emits('changeView', 'sessionpending')
   } else {
     alert('Please fill in all fields to book a session')
@@ -83,14 +108,14 @@ watch(time, (newTime, oldTime) => {
           <v-btn v-for="time in times" :key="time" @click="selectTime(time)">{{ time }}</v-btn>
         </v-btn-group>
       </div>
-      <div v-if="formattedDate && time" class="column">
+      <div v-if="toIsoDate && time" class="column">
         <p class="selected-datetime">
-          <span v-if="sessionsStore.selectedSite">{{ sessionsStore.selectedSite.site }} Selected for {{ formattedDate }} at {{ time }}</span>
-          <span v-else>Click on a pin to book for {{ formattedDate }} at {{ time }}</span>
+          <span v-if="sessionsStore.selectedSite">{{ sessionsStore.selectedSite.site }} Selected for {{ toIsoDate }} at {{ time }}</span>
+          <span v-else>Click on a pin to book for {{ toIsoDate }} at {{ time }}</span>
         </p>
         <v-btn variant="tonal" v-if="date && sessionsStore.selectedSite" @click="bookDate" class="blue-bg">Book</v-btn>
       </div>
-      <LeafletMap v-if="formattedDate && time" />
+      <LeafletMap v-if="toIsoDate && time" />
     </div>
   </div>
 </template>
