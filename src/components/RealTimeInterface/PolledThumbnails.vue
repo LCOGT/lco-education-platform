d3a2e7de8ee9edb7568b0845f4f4c00ec7315b89
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-07-03 16:34:22 -0700

    imported fetch api call

--- src/components/RealTimeInterface/PolledThumbnails.vue
+++ src/components/RealTimeInterface/PolledThumbnails.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useSessionsStore } from '../../stores/sessions'
+import { fetchApiCall } from '../../utils/api'
 
 const sessionsStore = useSessionsStore()
 const currentSession = sessionsStore.currentSession
+
 const sessionId = currentSession.id
 
 const thumbnails = ref([])
@@ -12,16 +14,13 @@ let pollingInterval = null
 
 const thumbnailsApiUrl = `http://archive-api-dev.lco.gtn/thumbnails/?observation_id=${sessionId}&size=large`
 
-function getThumbnails () {
-  fetch(thumbnailsApiUrl)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      thumbnails.value = data.results.map(result => result.url)
-    })
-    .catch(error => {
-      console.error('Error:', error)
-    })
+const saveThumbnails = (data) => {
+  thumbnails.value = data.results.map(result => result.url)
+}
+
+const getThumbnails = async () => {
+  // ADD AUTH TOKEN
+  await fetchApiCall({ url: thumbnailsApiUrl, method: 'GET', successCallback: saveThumbnails, failCallback: console.error })
 }
 
 onMounted(() => {
