90f05ee7a3f7dc81e1de20885dc89976bb6d6cea
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-07-03 12:07:58 -0700

    getting dates from session store for dashboard display purposes and sorting them in descending order from present - 16 minutes

--- src/components/Images/MyGallery.vue
+++ src/components/Images/MyGallery.vue
@@ -1,22 +1,34 @@
 <script setup>
 import thumbnail from '../../assets/TemporaryImages/thumbnail.png'
-import { ref } from 'vue'
+import { computed } from 'vue'
+import { useSessionsStore } from '../../stores/sessions'
 
-// TO DO: get images from api and sort them by
-const observations = ref([
-  { id: 1, date: 'May 19, 2024', telescope: 'Maui, LCO', num: 3 },
-  { id: 2, date: 'April 3, 2024', telescope: 'Queue', num: 4 },
-  { id: 3, date: 'March 13, 2024', telescope: 'Maui, LCO', num: 6 },
-  { id: 4, date: 'March 12, 2024', telescope: 'Queue', num: 4 }
-])
+const sessionsStore = useSessionsStore()
+
+const observations = computed(() => {
+  console.log(sessionsStore.sessions.results)
+  const now = new Date()
+  // Current time minus 16 minutes
+  const cutoffTime = new Date(now.getTime() - 16 * 60 * 1000)
+  return sessionsStore.sessions.results
+    .filter(obs => new Date(obs.start) < cutoffTime)
+    .slice().sort((a, b) => {
+      return new Date(b.start) - new Date(a.start)
+    })
+})
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString)
+  return new Intl.DateTimeFormat('en-US', { month: 'long', day: 'numeric', year: 'numeric' }).format(date)
+}
 
 </script>
 
 <template>
     <div class="container" v-for="obs in observations" :key="obs.id">
-        <h3>{{ obs.date }}</h3>
+        <h3>{{ formatDate(obs.start) }}</h3>
         <div class="columns is-multiline">
-            <div class="column is-one-quarter-desktop is-half-tablet" v-for="n in obs.num" :key="n">
+            <div class="column is-one-quarter-desktop is-half-tablet" v-for="id in obs" :key="id">
             <figure class="image is-square">
                 <img :src="thumbnail" class="thumbnail" />
             </figure>
