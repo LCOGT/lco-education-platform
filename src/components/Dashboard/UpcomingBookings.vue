7dd0e76cf2ba625c91b8ea8b7f99f4bb7ed433b5
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-07-10 19:28:03 -0700

    got rid of redirect functions and use the as one liners

--- src/components/Dashboard/UpcomingBookings.vue
+++ src/components/Dashboard/UpcomingBookings.vue
@@ -6,14 +6,7 @@ import { useSessionsStore } from '../../stores/sessions'
 const router = useRouter()
 const sessionsStore = useSessionsStore()
 
-const redirectToBooking = () => {
-  router.push('/book/realtime')
-}
-
-const redirectToScheduling = () => {
-  router.push('/schedule')
-}
-
+// change to bookings and add an icon to show completion
 const sortedSessions = computed(() => {
   const now = new Date().getTime()
   // TODO: Show past sessions for a certain amount of time in a separate section
@@ -39,6 +32,7 @@ const selectSession = (sessionId) => {
   router.push(`/realtime/${sessionId}`)
 }
 
+// move these two to a utils file
 const formatDate = (dateString) => {
   const date = new Date(dateString)
   const options = { year: 'numeric', month: 'long', day: 'numeric' }
@@ -66,7 +60,7 @@ onMounted(() => {
             <div><a @click.prevent="selectSession(session.id)">{{ formatDate(session.start) }}</a></div><div>{{ formatTime(session.start) }}</div>
         </div>
         </div>
-        <button class="button red-bg" @click="redirectToBooking"> Book Slot </button>
+        <button class="button red-bg" @click="router.push('/book/realtime')"> Book Slot </button>
     </div>
     <div class="observations">
         <h3>Scheduled Observations</h3>
@@ -75,7 +69,7 @@ onMounted(() => {
                 <div>{{ title }}</div><div><progress class="progress is-large is-primary" :value="progress" max="100">{{ progress }}%</progress></div>
             </div>
         </div>
-        <button class="button red-bg" @click="redirectToScheduling">Schedule Observations</button>
+        <button class="button red-bg" @click="router.push('/schedule')">Schedule Observations</button>
     </div>
 </template>
 
