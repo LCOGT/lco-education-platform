a7919424cb790a74175a6ba1dbf94ea4abd1f7d9
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-07-17 14:31:29 -0700

    Added configuration store

---
+++ src/stores/configuration.js
@@ -0,0 +1,11 @@
+import { defineStore } from 'pinia'
+
+export const useConfigurationStore = defineStore('configuration', {
+  state () {
+    return {
+      isConfigLoaded: false,
+      observationPortalUrl: '',
+      thumbnailArchiveUrl: ''
+    }
+  }
+})
