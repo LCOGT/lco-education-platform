6e09851f881be23fd042417231a09ffdf2a76836
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-08-09 16:14:33 -0700

    added emits to pass to session image capture when thumbnails has length so user can capture more images

--- src/components/RealTimeInterface/PolledThumbnails.vue
+++ src/components/RealTimeInterface/PolledThumbnails.vue
@@ -1,25 +1,43 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, defineEmits } from 'vue'
 import { useSessionsStore } from '../../stores/sessions'
 import { fetchApiCall } from '../../utils/api'
 import { useConfigurationStore } from '../../stores/configuration'
+import { useUserDataStore } from '../../stores/userData'
 
 const sessionsStore = useSessionsStore()
 const currentSession = sessionsStore.currentSession
-
+const userDataStore = useUserDataStore()
 const configurationStore = useConfigurationStore()
 
 const sessionId = currentSession.id
+
+const emits = defineEmits(['thumbnailsFetched'])
 
 // make this into a carousel (future)
 const thumbnails = ref([])
 let pollingInterval = null
 
 const getThumbnails = async () => {
-  // const token = `Token ${userDataStore.authToken}`
+  const token = userDataStore.authToken
+  const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': `Token ${token}`
+  }
   // ADD AUTH TOKEN
-  await fetchApiCall({ url: configurationStore.thumbnailArchiveUrl + `thumbnails/?observation_id=${sessionId}&size=large`, method: 'GET', successCallback: (data) => { thumbnails.value = data.results.map(result => result.url) }, failCallback: console.error })
-  console.log('thumbnails', thumbnails.value)
+  await fetchApiCall({
+    url: configurationStore.thumbnailArchiveUrl + `thumbnails/?observation_id=${sessionId}&size=large`,
+    method: 'GET',
+    header: headers,
+    successCallback: (data) => {
+      thumbnails.value = data.results.map(result => result.url)
+      if (thumbnails.value.length > 0) {
+        emits('thumbnailsFetched', true)
+      }
+    },
+    failCallback: console.error
+  })
 }
 
 onMounted(() => {
@@ -31,6 +49,6 @@ onMounted(() => {
 
 <template>
     <div>
-<img :src="thumbnails[thumbnails.length-1]" />
-</div>
+      <img :src="thumbnails[0]" />
+    </div>
 </template>
