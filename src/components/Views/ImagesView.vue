65f693f680696ddb0f185863e10339b9b67e62ed
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-08-28 18:09:22 -0700

    fixes issue where there was infinite scroll and rendering of images

--- src/components/Views/ImagesView.vue
+++ src/components/Views/ImagesView.vue
@@ -1,20 +1,10 @@
 <script setup>
 import MyGallery from '../Images/MyGallery.vue'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { ref } from 'vue'
-
-// TO DO: get images from api and sort them by
-const observations = ref([
-  { id: 1, date: 'May 19, 2024', telescope: 'Maui, LCO', num: 3 },
-  { id: 2, date: 'April 3, 2024', telescope: 'Queue', num: 4 },
-  { id: 3, date: 'March 13, 2024', telescope: 'Maui, LCO', num: 6 },
-  { id: 4, date: 'March 12, 2024', telescope: 'Queue', num: 4 }
-])
 
 </script>
 
 <template>
-    <div class="container" v-for="obs in observations" :key="obs.id">
+    <div class="container">
         <MyGallery />
     </div>
 </template>
