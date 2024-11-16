a5d684dfc467f021c480bb2a148686c0200d9aa8
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-11-15 16:05:06 -0800

    adds proposal dropdown component

---
+++ src/components/Global/ProposalDropdown.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+import { useUserDataStore } from '../../stores/userData'
+
+const userDataStore = useUserDataStore()
+const proposals = userDataStore.profile.proposals
+
+const selectedProposal = ref()
+
+</script>
+<template>
+    <div>
+    <label for="proposalSelect">Select a prosposal:</label>
+    <select id="proposalSelect" v-model="selectedProposal">
+      <option v-for="proposal in proposals" :key="proposal.id" :value="proposal.id">
+        {{ proposal.id }}
+      </option>
+    </select>
+  </div>
+</template>
