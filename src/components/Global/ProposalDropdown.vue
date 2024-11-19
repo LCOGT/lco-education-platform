442fde9f1552af6d387791fc9447257bead5ed50
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-11-18 16:37:42 -0800

    Adds proposal dropdown component

--- src/components/Global/ProposalDropdown.vue
+++ src/components/Global/ProposalDropdown.vue
@@ -1,20 +1,34 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch, defineEmits } from 'vue'
 import { useUserDataStore } from '../../stores/userData'
+
+const emit = defineEmits(['selectionsComplete'])
 
 const userDataStore = useUserDataStore()
 const proposals = userDataStore.profile.proposals
 
 const selectedProposal = ref()
 
+watch(selectedProposal, (newValue) => {
+  emit('selectionsComplete', newValue)
+})
+
 </script>
+
 <template>
+  <template v-if="proposals.length">
     <div>
-    <label for="proposalSelect">Select a prosposal:</label>
-    <select id="proposalSelect" v-model="selectedProposal">
-      <option v-for="proposal in proposals" :key="proposal.id" :value="proposal.id">
-        {{ proposal.id }}
-      </option>
-    </select>
-  </div>
+      <label for="proposalSelect">Select a proposal:</label>
+      <select id="proposalSelect" v-model="selectedProposal">
+        <option v-for="proposal in proposals" :key="proposal.id" :value="proposal.id">
+          {{ proposal.id }}
+        </option>
+      </select>
+    </div>
+  </template>
+  <template v-else>
+    <div>
+      <p>No proposals found. Please create a proposal <a href="https://observe.lco.global/apply">here</a></p>
+    </div>
+  </template>
 </template>
