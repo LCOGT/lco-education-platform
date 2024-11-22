7b42ce3cff5c14c0ba9969b56e170ab33f4290ee
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-11-21 16:38:19 -0800

    Adds route for observation details

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Scheduling from '../components/Views/SchedulingView.vue'
 import About from '../components/Views/AboutView.vue'
 import BookRTI from '../components/Views/BookRTIView.vue'
 import LogIn from '../components/LogIn/LogIn.vue'
+import ObservationDetailsView from '../components/Views/ObservationDetailsView.vue'
 
 const routes = [
   {
@@ -51,6 +52,11 @@ const routes = [
     path: '/login',
     name: 'LogIn',
     component: LogIn
+  },
+  {
+    path: '/observation/:id',
+    name: 'ObservationDetails',
+    component: ObservationDetailsView
   }
 ]
 
