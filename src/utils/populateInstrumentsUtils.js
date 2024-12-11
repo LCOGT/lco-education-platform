0165b80839441bb8751df2fd214928753a4be0a6
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-12-11 12:49:28 -0800

    adds getfilterlist utils

---
+++ src/utils/populateInstrumentsUtils.js
@@ -0,0 +1,20 @@
+import { fetchApiCall } from './api.js'
+import { useConfigurationStore } from '../stores/configuration.js'
+
+export const getFilterList = async () => {
+  const configurationStore = useConfigurationStore()
+  const response = await fetchApiCall({
+    url: `${configurationStore.observationPortalUrl}instruments`,
+    method: 'GET'
+  })
+  const filterList = []
+  Object.values(response).forEach((instrument) => {
+    if (instrument.class === '0m4' && instrument.optical_elements.filters) {
+      const schedulableFilters = instrument.optical_elements.filters
+        .filter(filter => filter.schedulable)
+        .map(filter => ({ name: filter.name, code: filter.code }))
+      filterList.push(...schedulableFilters)
+    }
+  })
+  return filterList
+}
