676282bdcd41555633b0e4ea427e0c92289c70c3
Carolina Capetillo <carolinacapetilloc@gmail.com>
2025-01-14 14:59:28 -0800

    adds targetnameentered

--- src/stores/skyCoordinates.js
+++ src/stores/skyCoordinates.js
@@ -3,12 +3,16 @@ import { defineStore } from 'pinia'
 export const useSkyCoordinatesStore = defineStore('coordinates', {
   state: () => ({
     ra: null,
-    dec: null
+    dec: null,
+    targetNameEntered: ''
   }),
   actions: {
     setCoordinates (ra, dec) {
       this.ra = ra
       this.dec = dec
+    },
+    setTargetNameEntered (value) {
+      this.targetNameEntered = value
     }
   }
 })
