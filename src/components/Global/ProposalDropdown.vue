51add7f939eedd509f585d3318d4ce6b5dd06dcd
Carolina Capetillo <carolinacapetilloc@gmail.com>
2025-02-06 09:35:59 -0800

    if there is only one proposal, it's automatically selected

--- src/components/Global/ProposalDropdown.vue
+++ src/components/Global/ProposalDropdown.vue
@@ -1,15 +1,35 @@
 <script setup>
-import { ref, defineEmits } from 'vue'
+import { ref, defineEmits, onMounted } from 'vue'
 import { useUserDataStore } from '../../stores/userData'
+import { fetchSemesterData, currentSemesterId } from '@/utils/calendarUtils'
+
 const emit = defineEmits(['selectionsComplete'])
+
 const userDataStore = useUserDataStore()
 const proposals = userDataStore.profile.proposals
 const activeProposals = proposals.filter(proposal => proposal.current === true)
 const selectedProposal = ref()
+const currentSemester = ref()
+
+const checkProposalsLength = () => {
+  // If there is only one proposal, automatically select it
+  if (activeProposals.length === 1) {
+    emit('selectionsComplete', proposals[0].id)
+  }
+}
+
+onMounted(() => {
+  checkProposalsLength()
+  fetchSemesterData().then(() => {
+    currentSemester.value = currentSemesterId
+    userDataStore.fetchRealTimeAllocations(currentSemester.value)
+  })
+})
 </script>
 
 <template>
-  <template v-if="proposals.length">
+  <!-- If there is more than one proposal, show the dropdown -->
+  <template v-if="proposals.length && proposals.length > 1">
     <div class="field">
       <label for="proposalSelect">Select the project you would like to use</label>
         <div class="control">
@@ -27,7 +47,8 @@ const selectedProposal = ref()
       </div>
     </div>
   </template>
-  <template v-else>
+  <!-- If there is less than one proposal, redirect user to creating a proposal -->
+  <template v-else-if="proposals.length < 1">
     <div>
       <p>No proposals found. Please create a proposal <a href="https://observe.lco.global/apply">here</a></p>
     </div>
