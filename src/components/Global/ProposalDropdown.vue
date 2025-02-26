2d28ab08b757849d84f3cd25aa99cc8a24e9ac01
Carolina Capetillo <carolinacapetilloc@gmail.com>
2025-02-26 13:58:56 -0800

    updates conditional rendering

--- src/components/Global/ProposalDropdown.vue
+++ src/components/Global/ProposalDropdown.vue
@@ -5,16 +5,12 @@ import { useProposalStore } from '../../stores/proposalManagement.js'
 const emit = defineEmits(['selectionsComplete'])
 const proposalStore = useProposalStore()
 const selectedProposal = ref()
-const moreThanOneProposalWithRealTimeAllocation = computed(() => proposalStore.proposalsWithRealTimeAllocation)
-const allActiveProposals = computed(() => proposalStore.allActiveProposals)
+const allActiveProposals = computed(() => { return proposalStore.allActiveProposals })
 
-onMounted(() => {
-  console.log('all active proposals', allActiveProposals.value)
-})
 </script>
 
 <template>
-  <template v-if="moreThanOneProposalWithRealTimeAllocation || allActiveProposals.length > 0">
+  <template v-if="allActiveProposals.length > 0">
     <div class="field">
       <label for="proposalSelect">Select the project you would like to use</label>
         <div class="control">
@@ -24,8 +20,8 @@ onMounted(() => {
             v-model="selectedProposal"
             @change="emit('selectionsComplete', selectedProposal)"
             >
-            <option v-for="proposal in allActiveProposals" :key="proposal.id" :value="proposal.id">
-                {{ proposal.title }}
+            <option v-for="proposal of allActiveProposals" :key="proposal.id" :value="proposal.id">
+              {{ proposal.title }}
             </option>
         </select>
         </div>
