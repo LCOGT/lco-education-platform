20393c0a5fbaa6ac0377eb8b748e163f75472189
Carolina Capetillo <carolinacapetilloc@gmail.com>
2025-02-27 12:26:56 -0800

    takes props to see if it's real time (view) and populate the correct proposal selections

--- src/components/Global/ProposalDropdown.vue
+++ src/components/Global/ProposalDropdown.vue
@@ -1,16 +1,39 @@
 <script setup>
-import { ref, defineEmits, onMounted, computed } from 'vue'
+import { ref, defineEmits, computed, defineProps, onMounted } from 'vue'
 import { useProposalStore } from '../../stores/proposalManagement.js'
 
 const emit = defineEmits(['selectionsComplete'])
+
+const props = defineProps({
+  isItRealTime: {
+    type: Boolean,
+    required: true
+  }
+})
+
 const proposalStore = useProposalStore()
 const selectedProposal = ref()
+const proposals = ref([])
 const allActiveProposals = computed(() => { return proposalStore.allActiveProposals })
+
+// If the user is requesting a real-time observation, only show proposals with real-time allocations
+// If the user is requesting a normal-time observation, only show proposals with normal time allocations
+const sortProposalDropdownSelection = () => {
+  if (props.isItRealTime) {
+    proposals.value = proposalStore.allActiveProposals
+  } else if (!props.isItRealTime) {
+    proposals.value = proposalStore.proposalsWithNormalTimeAllocation
+  }
+}
+
+onMounted(() => {
+  sortProposalDropdownSelection()
+})
 
 </script>
 
 <template>
-  <template v-if="allActiveProposals.length > 0">
+  <template v-if="allActiveProposals">
     <div class="field">
       <label for="proposalSelect">Select the project you would like to use</label>
         <div class="control">
@@ -20,7 +43,7 @@ const allActiveProposals = computed(() => { return proposalStore.allActivePropos
             v-model="selectedProposal"
             @change="emit('selectionsComplete', selectedProposal)"
             >
-            <option v-for="proposal of allActiveProposals" :key="proposal.id" :value="proposal.id">
+            <option v-for="proposal of proposals" :key="proposal.id" :value="proposal.id">
               {{ proposal.title }}
             </option>
         </select>
