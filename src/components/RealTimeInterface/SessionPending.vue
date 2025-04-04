34c24631c91ef31220f076ff14864cb3eb47bc8d
Carolina Capetillo <carolinacapetilloc@gmail.com>
2025-04-04 09:32:10 -0700

    removes commented out code and changes telescopemessage

--- src/components/RealTimeInterface/SessionPending.vue
+++ src/components/RealTimeInterface/SessionPending.vue
@@ -7,8 +7,6 @@ import WindyMap from './GlobeMap/WindyMap.vue'
 const realTimeSessionsStore = useRealTimeSessionsStore()
 
 const selectedSession = realTimeSessionsStore.currentSession
-// const availability = computed(() => realTimeSessionsStore.telescopeAvailability.event_type)
-// const reason = computed(() => realTimeSessionsStore.telescopeAvailability.event_reason)
 
 const lat = computed(() => sites[selectedSession.site]?.lat)
 const lon = computed(() => sites[selectedSession.site]?.lon)
@@ -17,7 +15,6 @@ const lon = computed(() => sites[selectedSession.site]?.lon)
 
 <template>
   <div class="content">
-    <!-- <p>The telescope is {{  availability  }} because {{  reason  }}</p> -->
     <WindyMap :lat="lat" :lon="lon"/>
   </div>
 </template>
