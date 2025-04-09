f5b4f9e844f84441fbf831b4e134af62c67d5c99
Carolina Capetillo <carolinacapetilloc@gmail.com>
2025-04-09 15:34:09 -0700

    Changes message

--- src/components/LogIn/LogIn.vue
+++ src/components/LogIn/LogIn.vue
@@ -71,12 +71,11 @@ const login = async () => {
             </span>
           </div>
         </div>
-
+        <p v-if="errorMessage">{{ errorMessage }}</p>
         <div class="field is-grouped">
           <div class="control">
             <input type="submit" id="login-btn" class="button is-link" value="Log me in">
           </div>
-          <p v-if="errorMessage">You have entered an invalid username or password</p>
           <div class="control">
             <button class="button is-link is-light">Cancel</button>
           </div>
