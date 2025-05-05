436598e33c7177dd946d78ddd7d5608e3ad22d20
Carolina Capetillo <carolinacapetilloc@gmail.com>
2025-05-05 16:14:37 -0700

    Renders scheduled obs details view

--- src/components/Views/ScheduledObsDetailsView.vue
+++ src/components/Views/ScheduledObsDetailsView.vue
@@ -1,9 +1,57 @@
 <script setup>
-import { computed, ref, onMounted, watch } from 'vue'
+import { computed, onMounted, defineProps } from 'vue'
 import { useObsPortalDataStore } from '../../stores/obsPortalData.js'
+
+const props = defineProps({
+  requestId: {
+    type: Number,
+    required: true
+  }
+})
+
+const obsPortalDataStore = useObsPortalDataStore()
+
+const observationDetails = computed(() => {
+  return obsPortalDataStore.observationDetails.results[0].requests
+})
+
+// const configurations = computed(() => {
+//   return obsPortalDataStore.observationDetails.results[0].requests[0].configurations
+// })
+
+onMounted(async () => {
+  const requestId = props.requestId
+  await obsPortalDataStore.fetchSelectedObservationDetails(requestId)
+})
 
 </script>
 
 <template>
-    <h1>HELLO</h1>
-</template>
+    <div v-if="observationDetails">
+        <h2>scheduled observation details</h2>
+      <div
+        v-for="(observation) in observationDetails"
+        :key="observation.id">
+        <div
+          v-for="(config) in observation.configurations"
+          :key="config.id">
+          <p>Target: {{ config.target.name }}</p>
+          <div
+            v-for="(exposure, expIndex) in config.instrument_configs"
+            :key="expIndex">
+            <p>Filter: {{ exposure.optical_elements.filter }}</p>
+            <p>Exposure Time: {{ exposure.exposure_time }}s</p>
+            <p>Exposure Count: {{ exposure.exposure_count }}</p>
+          </div>
+        </div>
+      </div>
+      <p>Start Date: {{ observationDetails[0].windows[0].start }}</p>
+      <p>End Date: {{ observationDetails[0].windows[0].end }}</p>
+    </div>
+
+    <div v-else>
+      <div>
+        <h1>No Observation Details Available</h1>
+      </div>
+    </div>
+  </template>
