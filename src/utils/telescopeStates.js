69e4e4e6917275d969eb49fb6cdd666e1bde97cd
Carolina Capetillo <carolinacapetilloc@gmail.com>
2025-06-02 15:16:39 -0700

    adds default for data if data is empty in telescope states request so that the user can proceed

--- src/utils/telescopeStates.js
+++ src/utils/telescopeStates.js
@@ -6,15 +6,17 @@ export const getTelescopeState = async (site, telescope, enclosure) => {
   // This needs to be wrapped in a promise to ensure that the data (which is async) is returned and handled in the realTimeSessions store
   return new Promise((resolve, reject) => {
     fetchApiCall({
-      url: `${configurationStore.observationPortalUrl}telescope_states/?site=${site}&telescope=${telescope}`,
+      url: `${configurationStore.observationPortalUrl}telescope_states/?site=${site}&telescope=${telescope}&enclosure=${enclosure}`,
       method: 'GET',
       successCallback: (response) => {
-        const data = response && response[`${site}.${enclosure}.${telescope}`][0]
+        let data
+        if (!response || !response[`${site}.${enclosure}.${telescope}`]) {
+          // Sometimes there is no response or the response is empty. In this case, we want the user to keep going with their rti session
+          data = 'Reason unavailable'
+        } else {
+          data = response && response[`${site}.${enclosure}.${telescope}`][0]
+        }
         resolve(data)
-      },
-      failCallback: (error) => {
-        console.error('Error fetching telescope state:', error)
-        reject(error)
       }
     })
   })
