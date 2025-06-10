051c999befc2718f6accca9dd7f8e226b59b3b1e
Edward Gomez <edward@gomez.me.uk>
2025-06-10 14:39:44 +0100

    Style updates to pending page

--- src/components/Views/RealTimeInterfaceView.vue
+++ src/components/Views/RealTimeInterfaceView.vue
@@ -1,18 +1,26 @@
 <script setup>
 import { ref, computed, onMounted, onBeforeUnmount, watch } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { useRealTimeSessionsStore } from '../../stores/realTimeSessions'
 import SessionPending from '../RealTimeInterface/SessionPending.vue'
 import SessionStarted from '../RealTimeInterface/SessionStarted.vue'
 import { formatCountdown, calculateSessionCountdown } from '../../utils/formatTime.js'
 import { useConfigurationStore } from '../../stores/configuration'
 import sites from '../../utils/sites.JSON'
+import oggFlag from '@/assets/Icons/ogg_flag.png'
+import cojFlag from '@/assets/Icons/coj_flag.png'
+import tfnFlag from '@/assets/Icons/tfn_flag.svg'
+import cptFlag from '@/assets/Icons/cpt_flag.svg'
+import elpFlag from '@/assets/Icons/elp_flag.png'
+import lscFlag from '@/assets/Icons/lsc_flag.png'
+import LeafletMap from '../RealTimeInterface/GlobeMap/LeafletMap.vue'
 
 const configurationStore = useConfigurationStore()
 const realTimeSessionsStore = useRealTimeSessionsStore()
 
 const timeRemaining = ref(0)
 const loading = ref(true)
-const telname = { '0m4a': 'Delta Rho', '0m4b': 'Delta Rho', '1m0a': '1 meter', '2m0a': '2 meter' }
+const telname = { '0m4a': 'Delta Rho 0.35m', '0m4b': 'Delta Rho 0.35m', '1m0a': '1 meter', '2m0a': '2 meter' }
 
 const selectedSession = realTimeSessionsStore.currentSession
 const site = computed(() => sites[selectedSession.site]?.name)
@@ -48,9 +56,49 @@ function countdown () {
   }, 1000)
 }
 
-const telescopeMessage = computed(
-  () => `The telescope's status is ${availability.value} because of ${reason.value}`
+const telescopeMessage = computed(() => {
+  let msg = ''
+  if (availability.value === 'AVAILABLE') {
+    msg = 'The telescope is currently available.'
+  } else if (availability.value === 'UNAVAILABLE' || availability.value === 'NOT_OK_TO_OPEN') {
+    msg = `The telescope is currently unavailable due to ${reason.value}.`
+  }
+  return msg
+}
 )
+
+const telescopeMsgState = computed(() => {
+  let msgstate = 'notification '
+  if (availability.value === 'AVAILABLE') {
+    msgstate += 'green-bg'
+  } else if (availability.value === 'UNAVAILABLE' || availability.value === 'NOT_OK_TO_OPEN') {
+    msgstate += 'is-danger'
+  }
+  return msgstate
+}
+)
+
+const siteFlag = computed(() => {
+  if (selectedSession) {
+    switch (selectedSession.site) {
+      case 'ogg':
+        return oggFlag
+      case 'coj':
+        return cojFlag
+      case 'tfn':
+        return tfnFlag
+      case 'cpt':
+        return cptFlag
+      case 'elp':
+        return elpFlag
+      case 'lsc':
+        return lscFlag
+      default:
+        return null
+    }
+  }
+  return null
+})
 
 watch(() => realTimeSessionsStore.currentStatus, (newStatus, oldStatus) => {
   if (newStatus === 'ACTIVE') {
@@ -88,18 +136,39 @@ onMounted(async () => {
       <div class="container">
        <div v-if="statusSessionNotActive" class="content">
           <h2>Session Not Started</h2>
-          <p><span class="green-bg px-2 py-2">Session starts in {{ formatCountdown(timeRemaining) }}</span></p>
-          <p>You are controlling the <strong>{{ telescope }}</strong> telescope in <strong>{{ site }}</strong></p>
-          <p>{{ telescopeMessage }}</p>
-          <SessionPending/>
+          <div class="columns">
+            <div class="column is-6">
+              <p><span class="blue-bg px-2 py-2">Session starts in {{ formatCountdown(timeRemaining) }}</span></p>
+              <div class="icon-text">
+                <span class="icon has-text-info">
+                  <FontAwesomeIcon icon="fa-regular fa-telescope"  />
+                </span>
+                <span>{{ telescope }}</span>
+              </div>
+              <h3>Current Status</h3>
+              <div :class="telescopeMsgState">{{ telescopeMessage }}</div>
+              <div class="icon-text">
+                <span class="icon has-text-info">
+                  <FontAwesomeIcon icon="fa-regular fa-location-dot"  />
+                </span>
+                <span>{{ site }}</span>
+              </div>
+
+              <img :src="siteFlag" alt="Site Flag" class="site-flag"/>
+
+            </div>
+            <div class="column is-6">
+              <SessionPending/>
+            </div>
+          </div>
         </div>
        <div v-else-if="(realTimeSessionsStore.currentStatus === 'ACTIVE' || configurationStore.demo == true)" class="content">
           <h2>Live Observing Session</h2>
           <p>
-            <span class="green-bg px-2 py-2">Time Remaining in session: {{ formatCountdown(timeRemaining) }}</span>
+            <span class="blue-bg px-2 py-2">Time Remaining in session: {{ formatCountdown(timeRemaining) }}</span>
           </p>
           <p>You are controlling the <strong>{{ telescope }}</strong> telescope in <strong>{{ site }}</strong></p>
-          <p>{{ telescopeMessage }}</p>
+          <div :class="telescopeMsgState">{{ telescopeMessage }}</div>
           <SessionStarted/>
         </div>
        <div v-else-if="timeRemaining <= 0 && (realTimeSessionsStore.currentStatus === 'EXPIRED' || realTimeSessionsStore.currentStatus === 'INACTIVE')">
@@ -116,5 +185,11 @@ onMounted(async () => {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+}
+
+.slot-info-box {
+  background-color: var(--grey-level4);
+  border-radius: 8px;
+  padding: 1rem;
 }
 </style>
